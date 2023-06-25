679d50aabe8f0cda3d654573542e6e673d3cb9af
Garvit Batra <batra.garvit28@gmail.com>
2023-06-25 17:35:53 +0530

    python file updated

--- mashup.py
+++ mashup.py
@@ -4,17 +4,22 @@ import sys
 from pydub import AudioSegment
 import json
 
-def downloadSongs(search_query,start_time,end_time):
+
+def downloadSongs(search_query, start_time, end_time):
     url = search_query
     if not os.path.exists('songs'):
         os.makedirs('songs')
-    subprocess.run(['yt-dlp',url,"--extract-audio","--audio-format", "mp3", "-o", f"songs/%(title)s.%(ext)s", "--postprocessor-args", f"-ss {start_time} -t {end_time - start_time}"])
+    subprocess.run(['yt-dlp', url, "--extract-audio", "--audio-format", "mp3", "-o",
+                   f"songs/%(title)s.%(ext)s", "--postprocessor-args", f"-ss {start_time} -t {end_time - start_time}"])
+
 
 def main():
     json_string = sys.argv[1]
     json_object = json.loads(json_string)
     for i in range(len(json_object)):
-        downloadSongs(json_object[i]["link"],int(json_object[i]["st"]),int(json_object[i]["et"]))
+        downloadSongs(json_object[i]["link"], int(
+            json_object[i]["st"]), int(json_object[i]["et"]))
+
 
 if __name__ == "__main__":
     main()
--- merge.py
+++ merge.py
@@ -2,18 +2,31 @@ import os
 import sys
 from pydub import AudioSegment
 
+
 def mergeSongs(output_file):
+    folder_path = "songs"
+    items = []
+    with os.scandir(folder_path) as entries:
+        for entry in entries:
+            creation_time = entry.stat().st_ctime
+            items.append((entry.name, creation_time))
+
+    sorted_items = sorted(items, key=lambda x: x[1])
+
+    merged_audio = AudioSegment.empty()
     folder = "songs"
-    audio_files = [f for f in os.listdir(folder) if f.endswith('.mp3')]
-    merged_audio = AudioSegment.from_mp3(os.path.join(folder, audio_files[0]))
-    for audio_file in audio_files[1:]:
-        audio = AudioSegment.from_mp3(os.path.join(folder, audio_file))
-        merged_audio = merged_audio + audio
+    for song in sorted_items:
+        song_path = f"{folder}/{song[0]}"
+        audio_segment = AudioSegment.from_file(song_path, format="mp3")
+        merged_audio += audio_segment
+
     merged_audio.export(output_file, format="mp3")
+
 
 def main():
     output_file = sys.argv[1]
     mergeSongs(output_file)
+
 
 if __name__ == "__main__":
     main()
